6d62b39c8587a02412bba6a45303b5a8cb0a3d34
oliverpople <oliverpople@gmail.com>
2018-02-06 11:41:13 +0000

    Passed test. App adds a new gift to `state` when clicking `add gift` button

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,6 +8,12 @@ it('renders correctly', () => {
   expect(app).toMatchSnapshot();
 });
 
-it("initializes the 'state' with an empty list of gifts", () => {
+it('initializes the `state` with an empty list of gifts', () => {
     expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
