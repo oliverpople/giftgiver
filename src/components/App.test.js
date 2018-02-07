d1dcda620a49973582331088631a9af0103edc3e
oliverpople <oliverpople@gmail.com>
2018-02-07 14:26:54 +0000

    Hardcoded test Gift id equal to 1. Passed Gift test - the user wants to remove the added gift….they removes the gift from `state

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,28 +11,41 @@ describe('App', () => {
   });
 
   it('initializes the `state` with an empty list of gifts', () => {
-      expect(app.state().gifts).toEqual([]);
+    expect(app.state().gifts).toEqual([]);
   });
 
   describe('when clicking the `add-gift` button', () => {
+    const id = 1;
+
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id }]);
+    });
+
+    it('adds new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+
+    it('creates a Gift component', () => {
+      expect(app.find('Gift').exists()).toBe(true);
+    });
+
+
+    describe('and the user wants to remove the added gift', () => {
       beforeEach(() => {
-        app.find('.btn-add').simulate('click');
+        app.instance().removeGift(id);
       });
 
-      afterEach(() => {
-        app.setState({ gifts: [] });
+      it('removes the gift from `state`', () => {
+        expect(app.state().gifts).toEqual([]);
       });
-
-      it('adds a new gift to `state`', () => {
-        expect(app.state().gifts).toEqual([{ id: 1 }]);
-      });
-
-      it('adds new gift to the rendered list', () => {
-        expect(app.find('.gift-list').children().length).toEqual(1);
-      });
-
-      it('creates a Gift component', () => {
-        expect(app.find('Gift').exists()).toBe(true);
-      });
+    });
   });
 });
