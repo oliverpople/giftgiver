e102301f20f253f6d5d5a498e225c8e5de30fd22
oliverpople <oliverpople@gmail.com>
2018-02-07 11:30:34 +0000

    Refactored App tests. Batch tests into scenarios, used the beforeEach() function the DRY up code and afterEach() function to prevent test pollution.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -14,15 +14,23 @@ describe('App', () => {
       expect(app.state().gifts).toEqual([]);
   });
 
-  it('adds a new gift to `state` when clicking `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
+  describe('when clicking the `add-gift` button', () => {
+      beforeEach(() => {
+        app.find('.btn-add').simulate('click');
+      });
 
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
+      afterEach(() => {
+        app.setState({ gifts: [] });
+      });
+
+      it('adds a new gift to `state`', () => {
+        expect(app.state().gifts).toEqual([{ id: 1 }]);
+      });
+
+      it('adds new gift to the rendered list', () => {
+        expect(app.find('.gift-list').children().length).toEqual(1);
+      });
   });
 
-  it('adds new gift to the rendered list when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
 
-    expect(app.find('.gift-list').children().length).toEqual(2);
-  });
 });
