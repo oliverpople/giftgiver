a43ee5f2f844082ca87c29c0dd291b264034c2c4
oliverpople <oliverpople@gmail.com>
2018-02-07 11:19:06 +0000

    Restructured App.test file and removed test pollution using Jest’s global describe() function.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('adds a new gift to `state` when clicking `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+  it('initializes the `state` with an empty list of gifts', () => {
+      expect(app.state().gifts).toEqual([]);
+  });
 
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  it('adds a new gift to `state` when clicking `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
 
-it('adds new gift to the rendered list when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+  });
 
-  expect(app.find('.gift-list').children().length).toEqual(2);
+  it('adds new gift to the rendered list when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.find('.gift-list').children().length).toEqual(2);
+  });
 });
